31447ed03e5427ec70eddaa991c02662bf7d25e6
Little-Ben <Little-Ben@users.noreply.github.com>
2019-05-11 23:53:22 +0200

    add NODESFILE_REMOTE and NODESFILE_LOCAL to enable remote nodes.json; SITE_CODE to filter in multi site communities

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ ffapi-updater helps to manage your Freifunk API file.
 ## Prerequisites
 The following files should be available on filesystem of server (if necessary 
 download them before execution, e.g. using wget):
-- nodes.json (v2) of Meshviewer's ffmap-backend
+- nodes.json (v2) of Meshviewer's ffmap-backend (could also be remote url)
 - Freifunk-API json file 
 
 ffapi-updater modifies your API file, so best practice is to run it on that 
@@ -12,6 +12,13 @@ server that hosts your API file. If this is not possible, you need to
 upload your API file after each execution to its place. In our case API and
 map files are hosted on the same server, so both needed json files could be 
 referenced directly.
+
+Since V1.2.0 it is possible to load a remote nodes.json (via url), simply 
+configure the complete url and no loacal nodes.json will be used.
+
+Furthermore SITE_CODE is checked, this helps to filter in multi site 
+communities.
+
 
 ## Installation
 To use it, simply clone this repository to a folder on your map server and 
--- ffapi-updater.py
+++ ffapi-updater.py
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------------
 #
 #    ffapi-updater - Update Freifunk API file
-#    Copyright (C) 2016 Benjamin Schmitt
+#    Copyright (C) 2016,2019 Benjamin Schmitt
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -21,22 +21,35 @@
 #    Source code available at: https://github.com/Little-Ben/ffapi-updater
 #
 # ------------------------------------------------------------------------------
-# This python script counts all active nodes (online = true) in NODEFILE
+# This python script counts all active nodes (online = true)
 # and updates the node count and lastchange date (UTC) in APIFILE.
+# The source is a meshviewer's nodes.json in version 2.
 # ------------------------------------------------------------------------------
+# Configuration:
+# - APIFILE is a local file containing your API-File,
+#   file must be writeable for executing user
+# - NODESFILE_REMOTE is a remote source
+#  (https://example.com/meshviewer/nodes.json)
+# - NODESFILE_LOCAL is a local source
+#   and will only be used if NODESFILE_REMOTE is not given (empty string '')
+# - SITE_CODE is the abbreviation of your community, i.e. 'ffwp'
+#  (it has to be given, and could be used as a filter in multi-community setups)
+#
 # Configuration start
 APIFILE = '../api/FreifunkWestpfalz-api.json'
-NODESFILE = '../ffmap-data/nodes.json'
-# Configuration end 
+NODESFILE_LOCAL = '../meshviewer/nodes.json'
+NODESFILE_REMOTE = ''
+SITE_CODE = 'ffwp'
+# Configuration end
 # ------------------------------------------------------------------------------
 
-VERSION = 'V1.1.0'
+VERSION = 'V1.2.0'
 
-import json
+import json,requests
 from pprint import pprint
 from datetime import datetime
 
-print("\nffapi-updater",VERSION,"Copyright (C) 2016 Benjamin Schmitt")
+print("\nffapi-updater",VERSION,"Copyright (C) 2016,2019 Benjamin Schmitt")
 print("----------------------------------------------------------------------")
 print("This program comes with ABSOLUTELY NO WARRANTY.")
 print("This is free software, and you are welcome to redistribute it,")
@@ -47,22 +60,37 @@ print("----------------------------------------------------------------------")
 with open(APIFILE) as data_file:
     data = json.load(data_file)
 
-with open(NODESFILE) as node_file:
-    dataNodes = json.load(node_file)
+if NODESFILE_REMOTE != "":
+    headers = {
+        'User-Agent': 'ffapi-updater by Little-Ben, ' + VERSION + ', used by: ' + SITE_CODE + ', see: https://github.com/Little-Ben/ffapi-updater'
+    }
 
-#count active nodes
+    r = requests.get(NODESFILE_REMOTE, headers=headers)
+    dataNodes = r.json()
+else:
+    with open(NODESFILE_LOCAL) as node_file:
+        dataNodes = json.load(node_file)
+
+#count data
 iNodeCount=0
+iNodeCountSite=0
+iNodeCountFile=0
 for node in dataNodes["nodes"]:
-    if node["flags"]["online"] == True:
-        iNodeCount = iNodeCount + 1
+    if node["nodeinfo"]["system"].get("site_code","n/a") == SITE_CODE:
+        if node["flags"].get("online",False) == True:
+            iNodeCount = iNodeCount + 1
+        iNodeCountSite = iNodeCountSite + 1
+    iNodeCountFile = iNodeCountFile + 1
 
 #data update
-print("UTC time:\t\t ", datetime.utcnow().strftime("%Y-%m-%dT%T.%f"))
+print("UTC time:\t\t\t ", datetime.utcnow().strftime("%Y-%m-%dT%T.%f"))
 iNodeCountOld=data["state"]["nodes"]
-print("node count old:\t\t ", str(iNodeCountOld))
+print("node count site/online old:\t ", str(iNodeCountOld))
 data["state"]["nodes"] = iNodeCount
 data["state"]["lastchange"] = datetime.utcnow().strftime("%Y-%m-%dT%T.%f")
-print("node count new:\t\t ", str(data["state"]["nodes"]))
+print("node count site/online new:\t ", str(data["state"]["nodes"]))
+print("node count site/all:\t\t ", str(iNodeCountSite))
+print("node count file/all:\t\t ", str(iNodeCountFile))
 
 if iNodeCountOld != iNodeCount:
     #write new api file, sorted and prettyprinted - only if node count changed
@@ -81,3 +109,6 @@ else:
 print(strEndMessage, "DONE.\n")
 
 exit(0)
+
+
+
